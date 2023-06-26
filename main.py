bae01e67f8e39f8f0b70b6d1d5c83f08a4a0ea52
Andrey Utkin <45770262+Abisual@users.noreply.github.com>
2023-06-26 20:55:50 +0500

    Update

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def validate_number(input_value):
     try:
         value = int(input_value)
         if value < 1:
-            raise ValueError("Число должно быть больше или равно 1")
+            raise ValueError("The number must be greater than or equal to 1")
     except ValueError:
         return False
 
     return True
 
 def select_files():
-    filenames = askopenfilenames(title="Выберите аудиофайлы", filetypes=[("Audio Files", "*.mp3 *.wav *.ogg")])
+    filenames = askopenfilenames(title="Select audio files", filetypes=[("Audio Files", "*.mp3 *.wav *.ogg")])
     return filenames
 
 def select_folder():
-    folder = askdirectory(title="Выберите папку для сохранения")
+    folder = askdirectory(title="Select a folder to save")
     return folder
 
 def split_audio(input_file, output_folder, chunk_duration, delete_input_files):
@@ -62,7 +62,7 @@ def process_files():
 
     chunk_duration_value = chunk_duration.get()
     if not validate_number(chunk_duration_value):
-        messagebox.showerror("Ошибка", "Введите корректное число больше или равное 1")
+        messagebox.showerror("Error", "Enter a valid number greater than or equal to 1")
         return
 
     for input_file in input_files:
@@ -74,19 +74,19 @@ def process_files():
     root.quit()
 
 root = Tk()
-root.title("Разделение аудиофайлов")
+root.title("Splitting audio files")
 
-label_duration = Label(root, text="Продолжительность фрагмента (в секундах):")
+label_duration = Label(root, text="Fragment duration (in seconds):")
 label_duration.grid(row=0, column=0, padx=10, pady=10)
 chunk_duration = Entry(root)
 chunk_duration.insert(0, "10")
 chunk_duration.grid(row=0, column=1, padx=10, pady=10)
 
 delete_var = BooleanVar()
-delete_checkbox = Checkbutton(root, text="Удалить исходные файлы", variable=delete_var)
+delete_checkbox = Checkbutton(root, text="Delete Source files", variable=delete_var)
 delete_checkbox.grid(row=1, column=0, columnspan=2)
 
-process_button = Button(root, text="Обработать файлы", command=process_files)
+process_button = Button(root, text="Process Files", command=process_files)
 process_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
 root.mainloop()
